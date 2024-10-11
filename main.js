f6219ee14f084677a1934ff156b21f11b2c5e7d1
gitmt14 <momna.tariq@torontomu.ca>
2024-10-10 23:32:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -15,4 +15,44 @@ window.onload = function() {
     var aboutMeHeading = document.getElementById("aboutMe");
     aboutMeHeading.style.fontSize = "30px";  // Increase font size of the About Me section
     aboutMeHeading.style.fontStyle = "italic";  // Change font style to italic
-};
+}// Lab 5
+// Prompt the user to enter their name when the button is clicked
+function askName() {
+    var userName = prompt("Please enter your name:");
+    if (userName) {
+        document.getElementById("welcome").innerHTML = "Welcome, " + userName + "!";
+    }
+}
+
+// Make the image clickable and enlarge when clicked
+function enlargeImage() {
+    var img = document.getElementById("profileImage");
+    if (img.style.width === "200px") {
+        img.style.width = "400px";  // Enlarge image
+    } else {
+        img.style.width = "200px";  // Reset to original size
+    }
+}
+
+// Toggle the dropdown menu
+function toggleDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if user clicks outside
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+// Change color on mouseover and mouseout for nav items
+function changeColorOnHover(element, color) {
+    element.style.color = color;
+}
